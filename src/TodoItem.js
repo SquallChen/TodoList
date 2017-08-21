c655a5b16bdb832243e9463e74ceb9b83d58e005
MR.C <442947365@qq.com>
2017-08-21 19:11:18 +0800

    完成基本功能和样式

---
+++ src/TodoItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './TodoItem.css'
+
+ 
+ export default class TodoItem extends Component {
+   render(){
+     return (
+     <div className="TodoItem">
+       <input type="checkbox" checked={this.props.todo.status ==='completed'}
+       onChange={this.toggle.bind(this)}/><span className="title">{this.props.todo.title}</span>
+       <span className="complete">{converSion(this.props.todo.status)}</span>
+       <button onClick={this.delete.bind(this)}>删除</button>
+     </div>
+     )
+   }
+ 
+    toggle(e){
+      this.props.onToggle(e,this.props.todo)
+     }
+     delete(e){
+       this.props.onDelete(e,this.props.todo)
+     }
+     
+    
+ }
+ function converSion(item){
+    if(item==='completed'){
+      return '已完成'
+    }else{
+      return '未完成'
+    }
+  }
